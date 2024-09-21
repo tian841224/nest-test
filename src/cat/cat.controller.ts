8d077a31a75bf3580838ba0442842961276bf179
Tian <jacky841224j@gmail.com>
2024-09-21 20:54:47 +0800

    feat：將cat改為存資料庫
    - 新增或調整項目：原為儲存成陣列，改為存到MySQL

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -1,53 +1,55 @@
-import { Body, Controller, Delete, Get, Logger, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import {Body, Controller, Delete,Get,Logger,NotFoundException,Param,Post,Put} from '@nestjs/common';
 import { CatService } from './cat.service';
-import { CatEntity } from '../entities/cat.entity';
-import { CatCreateDto } from '../dtos/cat/cat.create.dto';
-import { CatUpdateDto } from '../dtos/cat/cat.update.dto';
+import { Cat } from './entities/cat.entity';
+import { CatCreateDto } from './dtos/cat.create.dto';
+import { CatUpdateDto } from './dtos/cat.update.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('cats')
 @Controller('cat')
 export class CatController {
-    private readonly logger = new Logger(CatController.name);
+  private readonly logger = new Logger(CatController.name);
 
-    constructor(private readonly catService: CatService) {}
+  constructor(private readonly catService: CatService) {}
 
-    @Get()
-    @ApiOperation({ summary: '查找所有貓' })
-    findAll(): CatEntity[] {
-        this.logger.log('Finding all cats');
-        return this.catService.findAll();
+  @Get()
+  @ApiOperation({ summary: '查找所有貓' })
+  async findAll(): Promise<Cat[]> {
+    this.logger.log('Finding all cats');
+    return await this.catService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: '查找單個貓' })
+  async findOne(@Param('id') id: number): Promise<Cat> {
+    this.logger.log(`Finding cat with id ${id}`);
+    const cat = await this.catService.findOne(id);
+
+
+    if (!cat) {
+      throw new NotFoundException('Cat not found');
     }
+    return cat;
+  }
 
-    @Get(':id')
-    @ApiOperation({ summary: '查找單個貓' })
-    findOne(@Param('id') id: number): CatEntity {
-        this.logger.log(`Finding cat with id ${id}`);
-        const cat = this.catService.findOne(id);
-        if (!cat) {
-            throw new NotFoundException('Cat not found');
-        }
-        return cat;
-    }
+  @Post()
+  @ApiOperation({ summary: '建立貓' })
+  async create(@Body() dto: CatCreateDto): Promise<Cat> {
+    this.logger.log(`Creating cat with name ${dto.name}`);
+    return await this.catService.create(dto);
+  }
 
-    @Post()
-    @ApiOperation({ summary: '建立貓' })
-    create(@Body() dto: CatCreateDto): CatEntity {
-        this.logger.log(`Creating cat with name ${dto.name}`);
-        return this.catService.create(dto);
-    }
+  @Put(':id')
+  @ApiOperation({ summary: '更新貓' })
+  async update(@Param('id') id: number, @Body() dto: CatUpdateDto): Promise<Cat> {
+    this.logger.log(`Updating cat with id ${id}`);
+    return await this.catService.update(id, dto);
+  }
 
-    @Put(':id')
-    @ApiOperation({ summary: '更新貓' })
-        update(@Param('id') id: number, @Body() dto: CatUpdateDto): CatEntity {
-        this.logger.log(`Updating cat with id ${id}`);
-        return this.catService.update(id, dto);
-    }
-
-    @Delete(':id')
-    @ApiOperation({ summary: '刪除貓' })
-    delete(@Param('id') id: number): void {
-        this.logger.log(`Deleting cat with id ${id}`);
-        this.catService.delete(id);
-    }
+  @Delete(':id')
+  @ApiOperation({ summary: '刪除貓' })
+  async delete(@Param('id') id: number): Promise<void> {
+    this.logger.log(`Deleting cat with id ${id}`);
+    await this.catService.delete(id);
+  }
 }
