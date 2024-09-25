b360d95e7edc95b051a000d2482fa506c6c516fb
Tian <jacky841224j@gmail.com>
2024-09-25 22:23:33 +0800

    Feat:加入DTO參數驗證功能

--- src/cat/dtos/cat.update.dto.ts
+++ src/cat/dtos/cat.update.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
 
 export class CatUpdateDto {
   @ApiProperty({ example: 'Jennie' })
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({ example: 20 })
+  @IsNumber()
+  @IsNotEmpty()
   age: number;
 
   @ApiProperty({ example: 'None' })
+  @IsString()
+  @IsNotEmpty()
   breed: string;
 }
