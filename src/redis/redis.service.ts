ee383237d8a57bb9b1fcd5ef617fd65c4812cc15
Tian <jacky841224j@gmail.com>
2024-10-06 21:03:44 +0800

    modify：重新封裝RedisModule

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Inject, Logger } from '@nestjs/common';
 import { Redis } from 'ioredis';
-import { REDIS_CLIENT } from './redis.module';
+import { REDIS_CLIENT } from './redis.confing.module';
 
 @Injectable()
 export class RedisService {
