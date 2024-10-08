d3a04e472ef368c9eb263cc355e28b234703e570
Tian <jacky841224j@gmail.com>
2024-10-09 01:17:26 +0800

    modify：調整DB注入方法

--- src/modules/database.module.ts
+++ src/modules/database.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
